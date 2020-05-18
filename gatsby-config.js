f381af77e4d9024006774119d43cdab87b1de401
Max <max@Maxs-MacBook-Air.local>
2020-05-18 13:23:13 -0400

    added wordpress site to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,40 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: "wcpboston.eelab.space", // or your custom WordPress url
+        // The protocol. This can be http or https.
+        protocol: "https",
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com.
+        // Defaults to true.
+        auth: null,
+        useACF: true, // Allows to pull ACF fields
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems.
+        verboseOutput: false,
+        // Exclude specific routes using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]` will exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        excludedRoutes: ["/*/*/comments", "/yoast/**"],
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
+
   ],
 }
