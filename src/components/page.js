96111f8dbfc741b382f678cb47e3eedc7854c88d
Max <max@Maxs-MacBook-Air.local>
2020-05-19 16:49:39 -0400

    page template to use in gatsby-node.js

---
+++ src/components/page.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { graphql } from "gatsby"
+import he from 'he';
+
+import Layout from "./layout"
+import SEO from "./seo"
+
+const Page = (props) => {
+    const page = props.data.wordpressPage;
+    console.log('page', page);
+    return (
+      <>
+    <Layout>
+      <SEO title="Home" />
+      <h1>{page.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: page.content }} />
+    </Layout>
+      </>
+    )
+}
+
+export default Page
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    wordpressPage(id: { eq: $id }) {
+      title
+      content
+    }
+  }
+`
