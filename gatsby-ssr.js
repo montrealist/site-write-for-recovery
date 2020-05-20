41c650016a90999810ad5738fe4d5d40c5d028a5
Max <max@Maxs-MacBook-Air.local>
2020-05-19 20:00:01 -0400

    load jquery with gatsby-ssr.sj

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+    setHeadComponents([
+        <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossOrigin="anonymous"></script>,
+    ])
+}
