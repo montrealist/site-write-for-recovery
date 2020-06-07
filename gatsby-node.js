61081a82727c446d9112e352013f06ed9853f3d6
Max <184944+montrealist@users.noreply.github.com>
2020-06-06 21:24:14 -0400

    subqueries and separate page templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,37 +11,93 @@ const { slash } = require(`gatsby-core-utils`)
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions
 
-  // query content for WordPress posts
-  const result = await graphql(`
+  const childrenOfHome = await graphql(`
     query {
-      allWordpressPage {
+      allWordpressPage(filter: {parent_element: {title: {}, slug: {eq: "home"}}}) {
         edges {
           node {
-            id
-            slug
             path
+            title
           }
         }
       }
     }
-  `);
+  `).then(result => result.data);
 
-  const postTemplate = path.resolve(`./src/components/page.js`)
-  result.data.allWordpressPage.edges.forEach(edge => {
-    console.log('page slug', edge);
-    createPage({
-      // will be the url for the page
-      path: edge.node.path,
-      // specify the component template of your choice
-      component: slash(postTemplate),
-      // In the ^template's GraphQL query, 'id' will be available
-      // as a GraphQL variable to query for this posts's data.
-      context: {
-        id: edge.node.id,
-      },
-    })
-  });
+  if (childrenOfHome.errors) throw new Error(childrenOfHome.errors);
 
+  // console.log('childrenOfHome: ', childrenOfHome.allWordpressPage.edges);
+
+  await Promise.all(childrenOfHome.allWordpressPage.edges.map(async (edge) => {
+    const pagePath = edge.node.path;
+    console.log('edge: ', pagePath);
+    const res = await graphql(`
+      query {
+        allWordpressPage(filter: {path: {glob: "${pagePath}*"}}) {
+          edges {
+            node {
+              id
+              slug
+              path
+            }
+          }
+        }
+      }
+    `);
+    // TODO: generate pages with a different template:
+
+    console.log('result for subquery: ', res.data.allWordpressPage.edges);
+
+    console.log('path', `./src/components${pagePath}page.js`);
+    const postTemplate = path.resolve(`./src/components${pagePath}page.js`);
+
+    console.log('postTemplate', postTemplate);
+
+    res.data.allWordpressPage.edges.forEach(edge => {
+      console.log('edge.node.path', edge.node.path);
+      createPage({
+        // will be the url for the page
+        path: edge.node.path,
+        // specify the component template of your choice
+        component: slash(postTemplate),
+        // In the ^template's GraphQL query, 'id' will be available
+        // as a GraphQL variable to query for this posts's data.
+        context: {
+          id: edge.node.id,
+        },
+      })
+    });
+  }));
+
+  // const result = await graphql(`
+  //   query {
+  //     allWordpressPage {
+  //       edges {
+  //         node {
+  //           id
+  //           slug
+  //           path
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+
+  // const postTemplate = path.resolve(`./src/components/page.js`)
+  // result.data.allWordpressPage.edges.forEach(edge => {
+  //   // console.log('page slug', edge);
+  //   createPage({
+  //     // will be the url for the page
+  //     path: edge.node.path,
+  //     // specify the component template of your choice
+  //     component: slash(postTemplate),
+  //     // In the ^template's GraphQL query, 'id' will be available
+  //     // as a GraphQL variable to query for this posts's data.
+  //     context: {
+  //       id: edge.node.id,
+  //     },
+  //   })
+  // });
 
   // FIXME: remove 'write-for-recovery' later!
   createRedirect({
